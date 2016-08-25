6d6664b5a7700f4329c37a8649a8ae0145685915
790036 <790036@seq.org>
2016-08-25 08:28:51 -0700

    Add files via upload

---
+++ FactCalc.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class FactCalc here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FactCalc
+{
+    private int tbf;
+    private int thenum;
+    public FactCalc(int a){
+        tbf = a;
+        thenum = 1;
+    }
+    public int factor(){
+        while(tbf>0){
+            thenum = tbf * thenum;
+            tbf = tbf - 1;
+        }
+        return thenum;
+
+    }
+}
---
+++ FactCalcRunner.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class FactCalcRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FactCalcRunner
+{
+    public static void main(String[] args){
+       FactCalc fc = new FactCalc(12);
+       System.out.print("The factorial is "+ fc.factor());
+    }
+}
